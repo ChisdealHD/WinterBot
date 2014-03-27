e8b0b20148e6660db34ba017501d67fba64abb41
DarkAutumn <clay.culver@gmail.com>
2014-03-26 23:46:00 -0700

    Added insult commands
    Added insult command.

--- WinterBot/WinterExtensions/Program.cs
+++ WinterBot/WinterExtensions/Program.cs
@@ -42,6 +42,7 @@ namespace WinterExtensions
             bot.AddCommands(new JukeBox(bot));
             bot.AddCommands(new Betting(bot));
             bot.AddCommands(new ViewerCountLogger(bot));
+            bot.AddCommands(new Insult(bot));
 
             bot.ModeratorRemoved += delegate(WinterBot b, TwitchUser user) { WriteLine("Moderator removed: {0}", user.Name); };
             bot.ModeratorAdded += delegate(WinterBot b, TwitchUser user) { WriteLine("Moderator added: {0}", user.Name); };
